6147833f31167f10a1a4dfac977b7cb08d0d668d
khairul ahmed <md.raju130@yahoo.com>
2022-10-25 08:00:07 -0700

    cousrsCategories added

--- index.js
+++ index.js
@@ -5,14 +5,39 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
-const news = require('./data/news.json');
+const categories = require('./data/courseCatagories.json');
+const products = require('./data/products.json');
 
 app.get('/', (req, res) => {
-    res.send('News a Running');
+    res.send('News API Running');
 });
 
-app.get('/news-', (req, res) => {
+app.get('/courses-categories', (req, res) => {
     res.send(categories)
 });
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '07') {
+        res.send(products);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+
+app.get('/products', (req, res) =>{
+    res.send(products);
+});
+
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+});
+
+app.listen(port, () => {
+    console.log('products Server running on port', port);
+})
 
