f6edb9057cee671d73fa82e010e20669b1b22830
khairul ahmed <md.raju130@yahoo.com>
2022-10-26 09:19:14 -0700

    categories added

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/courseCatagories.json');
-const products = require('./data/products.json');
+const news = require('./data/products.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running');
 });
 
-app.get('/courses-categories', (req, res) => {
+app.get('/news-categories', (req, res) => {
     res.send(categories)
 });
 
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
     if (id === '07') {
-        res.send(products);
+        res.send(news);
     }
     else {
         const category_news = news.filter(n => n.category_id === id);
@@ -27,17 +27,16 @@ app.get('/category/:id', (req, res) => {
     }
 })
 
-app.get('/products', (req, res) =>{
-    res.send(products);
+app.get('/news', (req, res) =>{
+    res.send(news);
 });
 
-app.get('/products/:id', (req, res) => {
+app.get('/news/:id', (req, res) => {
     const id = req.params.id;
     const selectedNews = news.find(n => n._id === id);
     res.send(selectedNews);
 });
 
 app.listen(port, () => {
-    console.log('products Server running on port', port);
+    console.log('Dragon News Server running on port', port);
 })
-
